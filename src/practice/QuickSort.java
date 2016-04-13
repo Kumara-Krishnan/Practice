a7eef5cf17f96f3b44c8c371652e026f3ce420f5
Kumara Krishnan <kumar.krish1210@gmail.com>
2016-04-13 23:42:26 +0530

    Quick Sort Imp :clap:  :boom: :alien:

---
+++ src/practice/QuickSort.java
@@ -0,0 +1,72 @@
+package practice;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * Partition algorithm for quick sort. Works only for an array with distinct
+ * elements.
+ *
+ *
+ *
+ *
+ * @author kumara krishnan
+ */
+public class QuickSort {
+
+    int p;
+    int q;
+    int r;
+
+    public void printArray(int[] arr) {
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+
+    public void quickSort(int[] arr, int p, int r) {
+
+        if (p < r) {
+            q = partition(arr, p, r);
+            //System.out.println("p " + p + " q " + (q - 1));
+            //System.out.println("q " + (q + 1) + " r " + r);
+            quickSort(arr, q + 1, r);
+            quickSort(arr, p, q - 1);
+        }
+    }
+
+    public int partition(int[] arr, int p, int r) {
+        int i = p + 1;
+        int temp;
+        //System.out.println("fuck this shit");
+        //System.out.println("p " + p + " r " + r);
+        for (int j = i; j <= r; j++) {
+            if (arr[j] < arr[p]) {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++;
+            }
+        }
+        temp = arr[i - 1];
+        arr[i - 1] = arr[p];
+        arr[p] = temp;
+        //printArray(arr);
+        //System.out.println();
+        //System.out.println("the pivot  " + (i - 1));
+        return i - 1;
+    }
+
+    static int[] arr = {5,4,3,2,4,5,6,-1,45,-24,5,3,2};
+
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+        qs.quickSort(arr, 0, arr.length-1);
+        qs.printArray(arr);
+    }
+}
